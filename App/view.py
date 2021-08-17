8a39d424ac938945c14902195388149c433944f9
df.cortesc <df.cortesc1@uniandes.edu.co>
2021-08-16 19:29:03 -0500

    Prueba

--- App/view.py
+++ App/view.py
@@ -42,6 +42,7 @@ def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
+    print("3")
     # TODO: Modificaciones para el laboratorio 1.
     print("0- Salir")
 
